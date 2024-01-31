cf00775f6393bf7ef045bca94430b48ccd89ccf8
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-01-31 08:56:10 -0600

    Setup rtk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,8 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import { Provider } from 'react-redux';
+import { store } from '@/state/store'
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -49,10 +51,12 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+      <Provider store={store}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        </Stack>
+      </Provider>
     </ThemeProvider>
   );
 }

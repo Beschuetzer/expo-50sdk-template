7ae9e9cbacc9ee87c965d579b72e1210f2d5211d
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-02-02 08:34:39 -0600

    Fix issue with camera not resetting properly

--- app/(tabs)/ScannerScreen.tsx
+++ app/(tabs)/ScannerScreen.tsx
@@ -1,25 +1,38 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator } from "react-native";
+import React, { useState, useCallback } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Camera, CameraType } from "expo-camera";
 import { Button } from "native-base";
 import { useRequestCameraPermissions } from "@/components/hooks/useRequestCameraPermissions";
-import { COLORS } from "@/constants/Colors";
 import { FullscreenSpinner } from "@/components/FullscreenSpinner";
+import { useFocusEffect } from "expo-router";
 
 const BarcodeScannerScreen = () => {
   const [type, setType] = useState(CameraType.back);
   const [scanned, setScanned] = useState(false);
   const hasPermission = useRequestCameraPermissions();
+  const [shouldRenderCamera, setShouldRenderCamera] = useState(true);
 
   const onSwitchCameraPress = useCallback(() => {
-    setType((current) => current === CameraType.back ? CameraType.front : CameraType.back);
-  }, [])
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }, []);
 
   const handleBarCodeScanned = useCallback((scannedObj: any) => {
     const { data } = scannedObj;
     setScanned(true);
     alert(`Bar code '${data}' has been scanned!`);
   }, []);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      setShouldRenderCamera(true);
+      return () => {
+        setScanned(false);
+        setShouldRenderCamera(false);
+      };
+    }, [])
+  );
 
   if (hasPermission === null) {
     return <FullscreenSpinner />;
@@ -30,19 +43,21 @@ const BarcodeScannerScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Camera
-        style={styles.camera}
-        type={type}
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-      />
-      {scanned && (
+      {shouldRenderCamera ? (
+        <Camera
+          style={styles.camera}
+          type={type}
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        />
+      ) : null}
+      {scanned ? (
         <TouchableOpacity
           style={styles.scanAgainButton}
           onPress={() => setScanned(false)}
         >
           <Text style={styles.scanAgainText}>Tap to Scan Again</Text>
         </TouchableOpacity>
-      )}
+      ) : null}
       <Button onPress={onSwitchCameraPress}>Switch Camera</Button>
     </View>
   );
@@ -57,7 +72,7 @@ const styles = StyleSheet.create({
   },
   scanAgainButton: {
     position: "absolute",
-    bottom: 20,
+    top: "50%",
     left: "25%",
     padding: 15,
     backgroundColor: "rgba(0, 0, 0, 0.5)",

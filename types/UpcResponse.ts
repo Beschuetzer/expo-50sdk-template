f375587126aa3aee8fef2ae336e0f1d377db98c0
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-02-02 12:05:34 -0600

    Add UpcResponse

---
+++ types/UpcResponse.ts
@@ -0,0 +1,26 @@
+/**
+*There are many more fields, just using the relevant ones for now.
+*See {@link https://world.openfoodfacts.org/api/v0/product/072273487253 this}.
+**/
+export type UpcResponse = {
+  code: number;
+  product: UpcProduct;
+  status: number;
+  status_verbose: string;
+};
+
+export type UpcProduct = {
+  image_front_small_url?: string;
+  image_front_thumb_url?: string;
+  image_front_url?: string;
+  image_ingredients_small_url?: string;
+  image_ingredients_thumb_url?: string;
+  image_ingredients_url?: string;
+  image_nutrition_small_url?: string;
+  image_nutrition_thumb_url?: string;
+  image_nutrition_url?: string;
+  image_small_url?: string;
+  image_thumb_url?: string;
+  image_url?: string;
+  product_name: string;
+};

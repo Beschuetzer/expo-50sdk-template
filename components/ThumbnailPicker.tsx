bb412f91b879ab088235a7c42bbd6350de7d4d15
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-02-02 20:27:41 -0600

    Add ThumbnailPicker

---
+++ components/ThumbnailPicker.tsx
@@ -0,0 +1,65 @@
+import { UpcProduct } from "@/types/UpcResponse";
+import { Row, AspectRatio, Image, View } from "native-base";
+import { useCallback, useMemo } from "react";
+import { TouchableOpacity } from "react-native";
+
+type ThumbnailPickerProps = {
+  upcProduct: UpcProduct;
+  selectedUrl: string;
+  setSelectedUrl: React.Dispatch<React.SetStateAction<string>>;
+};
+
+//todo: should the image be saved to AsyncStorage here?
+//todo: figure out how to use new Set for imagesToRender
+export function ThumbnailPicker(props: ThumbnailPickerProps) {
+  const { upcProduct, selectedUrl, setSelectedUrl } = props;
+
+  const imagesToRender = useMemo(
+    () => [
+      upcProduct.image_front_thumb_url,
+      upcProduct.image_ingredients_thumb_url,
+      upcProduct.image_thumb_url,
+      upcProduct.image_nutrition_url,
+    ],
+    [upcProduct]
+  );
+
+  const handleSelect = useCallback((imageUrl?: string) => {
+    if (!imageUrl) return;
+    setSelectedUrl(imageUrl);
+  }, [])
+
+  return (
+    <Row>
+      {imagesToRender.map((imageUrl) => {
+        const isSelected = imageUrl === selectedUrl;
+        console.log({imageUrl, selectedUrl, isSelected});
+        
+        return (
+          <View borderWidth={isSelected ? 2 : 0} borderColor={isSelected ? 'tertiary.900' : 'primary.900'}>
+            <TouchableOpacity onPress={() => handleSelect(imageUrl)}>
+              <AspectRatio
+                ratio={{
+                  base: 3 / 4,
+                  md: 9 / 10,
+                }}
+                height={{
+                  base: 100,
+                  md: 75,
+                }}
+              >
+                <Image
+                  resizeMode="cover"
+                  source={{
+                    uri: imageUrl,
+                  }}
+                  alt="Thumbnail"
+                />
+              </AspectRatio>
+            </TouchableOpacity>
+          </View>
+        );
+      })}
+    </Row>
+  );
+}

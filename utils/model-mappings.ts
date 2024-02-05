b18a9b89b3d7e79c65f9e575256ae1114290942a
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-02-04 20:14:44 -0600

    Add model-mapping

---
+++ utils/model-mappings.ts
@@ -0,0 +1,18 @@
+import { EMPTY_STRING } from "@/constants/general";
+import { Item } from "@/types/Item";
+import { UpcProduct } from "@/types/UpcResponse";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export async function getItem(input: UpcProduct): Promise<Item> {
+  return {
+    frequency: 0,
+    imageUri: {
+      url: input.image_front_thumb_url || input.image_thumb_url || EMPTY_STRING,
+      location:
+        (await AsyncStorage.getItem(input.code || EMPTY_STRING)) ||
+        EMPTY_STRING,
+    },
+    name: input.product_name,
+    upc: input.code || input.id,
+  };
+}

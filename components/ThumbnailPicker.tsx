f126b86e3e6077038ee5861a3b703d926a1d51ae
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-02-08 12:40:46 -0600

    Add button for taking own image

--- components/ThumbnailPicker.tsx
+++ components/ThumbnailPicker.tsx
@@ -1,49 +1,49 @@
-import { Row, View } from "native-base";
-import { useCallback, useMemo } from "react";
-import { TouchableOpacity } from "react-native";
+import { Center, Icon, Row, View, theme } from 'native-base'
+import { useCallback, useMemo } from 'react'
+import { TouchableOpacity } from 'react-native'
 
-import { ImageRenderer } from "./ImageRenderer";
-import { useIsDarkMode } from "./hooks/useIsDarkTheme";
+import { ImageRenderer } from './ImageRenderer'
+import { useIsDarkMode } from './hooks/useIsDarkTheme'
 
-import { UpcProduct } from "@/types/UpcResponse";
-import { StyleProp } from "@/types/general";
-import { getImagesFromUpcProduct } from "@/utils/helpers";
+import { UpcProduct } from '@/types/UpcResponse'
+import { StyleProp } from '@/types/general'
+import { getImagesFromUpcProduct } from '@/utils/helpers'
+import { Feather } from '@expo/vector-icons'
 
 type ThumbnailPickerProps = {
-  upcProduct: UpcProduct;
-  selectedUrl: string;
-  onSelectImage: (url: string) => void;
-} & StyleProp;
+  upcProduct: UpcProduct
+  selectedUrl: string
+  onSelectImage: (url: string) => void
+} & StyleProp
 
 export function ThumbnailPicker(props: ThumbnailPickerProps) {
-  const { upcProduct, selectedUrl, onSelectImage, style } = props;
-  const isDarkMode = useIsDarkMode();
+  const { upcProduct, selectedUrl, onSelectImage, style } = props
+  const isDarkMode = useIsDarkMode()
+  const modeColor = useMemo(() => isDarkMode
+            ? theme.colors.black
+            : theme.colors.white, [theme])
   const imagesToRender = useMemo(
     () => new Set(getImagesFromUpcProduct(upcProduct)),
     [upcProduct],
-  );
+  )
 
   const handleSelect = useCallback(
     (imageUrl?: string) => {
-      if (!imageUrl) return;
-      onSelectImage && onSelectImage(imageUrl);
+      if (!imageUrl) return
+      onSelectImage && onSelectImage(imageUrl)
     },
     [onSelectImage],
-  );
+  )
+
 
   return (
     <Row space={1} style={style}>
       {Array.from(imagesToRender).map((imageUrl) => {
-        const isSelected = imageUrl === selectedUrl;
+        const isSelected = imageUrl === selectedUrl
+        const borderColor = isSelected ? theme.colors.tertiary[900] : modeColor
 
         return (
-          <View
-            key={imageUrl}
-            borderWidth={2}
-            borderColor={
-              isSelected ? "tertiary.900" : isDarkMode ? "black" : "white"
-            }
-          >
+          <View key={imageUrl} borderWidth={2} borderColor={borderColor}>
             <TouchableOpacity onPress={() => handleSelect(imageUrl)}>
               <ImageRenderer
                 source={imageUrl}
@@ -53,8 +53,17 @@ export function ThumbnailPicker(props: ThumbnailPickerProps) {
               />
             </TouchableOpacity>
           </View>
-        );
+        )
       })}
+      <TouchableOpacity onPress={() => alert('pressed')}>
+        <Center borderColor={modeColor} width={75} height={100}>
+          <Feather name="plus" size={50} color={theme.colors.black} />
+        </Center>
+      </TouchableOpacity>
     </Row>
-  );
+  )
 }
+
+
+
+

003bb672d035d8d83b8bc0497d686ddd4ebcbbf9
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-02-25 15:11:53 -0600

    update tab view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,55 @@
-import { useState } from 'react'
-import { useWindowDimensions } from 'react-native'
-import { SceneMap, TabView } from 'react-native-tab-view'
-import { useDispatch } from 'react-redux'
+import { useMemo, useState } from 'react';
+import { useWindowDimensions } from 'react-native';
+import { SceneMap, TabView } from 'react-native-tab-view';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { useGpsCoordinate } from '@/components/hooks/useGeoLocation'
-import { InCartList } from '@/components/lists/InCartList'
-import { ShoppingList } from '@/components/lists/ShoppingLIst'
-import { setCurrentLocation } from '@/state/slices/listsSlice'
+import { useGpsCoordinate } from '@/components/hooks/useGeoLocation';
+import { InCartList } from '@/components/lists/InCartList';
+import { ShoppingList } from '@/components/lists/ShoppingLIst';
+import {
+  ListName,
+  setCurrentLocation,
+  storeSpecificListSelector,
+} from '@/state/slices/listsSlice';
 
 const renderScene = SceneMap({
   first: () => <ShoppingList />,
   second: () => <InCartList />,
-})
+});
 
 export default function TabOneScreen() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useGpsCoordinate({
     onSuccess: (gpsCoordinate) => {
-      dispatch(setCurrentLocation(gpsCoordinate))
+      dispatch(setCurrentLocation(gpsCoordinate));
     },
-  })
-  const layout = useWindowDimensions()
+  });
+  const layout = useWindowDimensions();
+  const shoppingList = useSelector(
+    storeSpecificListSelector(ListName.ShoppingList),
+  );
+  const inCartList = useSelector(
+    storeSpecificListSelector(ListName.InCartList),
+  );
 
-  const [index, setIndex] = useState(0)
-  const [routes] = useState([
-    { key: 'first', title: 'Need' },
-    { key: 'second', title: 'In Cart' },
-  ])
+  const [index, setIndex] = useState(0);
+  const routes = useMemo(
+    () => [
+      {
+        key: 'first',
+        title:
+          shoppingList.length > 0 ? `Need ${shoppingList.length}` : `Finished`,
+      },
+      {
+        key: 'second',
+        title:
+          inCartList.length > 0
+            ? `In Cart (${inCartList.length})`
+            : `Cart Empty`,
+      },
+    ],
+    [inCartList.length, shoppingList.length],
+  );
 
   return (
     <TabView
@@ -35,5 +58,5 @@ export default function TabOneScreen() {
       onIndexChange={setIndex}
       initialLayout={{ width: layout.width }}
     />
-  )
+  );
 }

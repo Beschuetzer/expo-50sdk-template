b38df14c891acd6d220642fc9d7d78f885e87d3a
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-03-27 07:00:17 -0500

    add new file

---
+++ components/lists/PreviouslyPurchasedItems.tsx
@@ -0,0 +1,188 @@
+import { FontAwesome } from '@expo/vector-icons';
+import { FlashList } from '@shopify/flash-list';
+import { Text, useTheme, Stack } from 'native-base';
+import React, { useCallback, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { ItemTileForRecommendedItems } from './ItemTileForRecommendedItems';
+import { ListItemSeparator } from './ListItemSeparator';
+import { SwipeableRow } from './SwipeableRow';
+
+import { FORM_INTER_ITEM_SPACING } from '@/constants/general';
+import {
+  currentStoreSelector,
+  updateStoreSpecificValues,
+  itemsPurchasedAtStoreSelector,
+  updateSelectedItemsFromPreviouslyPurchased,
+  setIsMultiSelectModeForPreviouslyPurchased,
+  selectedItemsFromPreviouslyPurchasedSelector,
+  ListName,
+  lastPurchasedMapSelector,
+  isMultiSelectModeForPreviouslyPurchasedSelector,
+} from '@/state/slices/listsSlice';
+import { ItemWithStoreSpecificValues, Key } from '@/types/Item';
+import { ListRow } from '@/types/general';
+import { getKeyToUse } from '@/utils/helpers';
+
+type PreviouslyPurchasedListProps = object;
+
+const listName: ListName = ListName.PreviouslyPurchased;
+/**
+ *The items for this list are actually not in the recommendations.data POS in redux.
+ *Instead they are calculated based on the storeSpecificValuesMap in the storeSpecificListSelector
+ **/
+export function PreviouslyPurchasedList(props: PreviouslyPurchasedListProps) {
+  const itemsPurchasedAtStore = useSelector(itemsPurchasedAtStoreSelector);
+  const lastPurchasedMap = useSelector(lastPurchasedMapSelector);
+  const currentStore = useSelector(currentStoreSelector);
+  const theme = useTheme();
+  const dispatch = useDispatch();
+  const listRef = useRef<FlashList<ItemWithStoreSpecificValues>>(null);
+  const [refreshing, setRefreshing] = useState(false);
+  const selectedItems = useSelector(
+    selectedItemsFromPreviouslyPurchasedSelector,
+  );
+  const isMultiSelectMode = useSelector(
+    isMultiSelectModeForPreviouslyPurchasedSelector,
+  );
+
+  const onSwipeRight = useCallback((key: Key) => {
+    setRefreshing(false);
+    dispatch(
+      updateStoreSpecificValues({
+        key,
+        storeSpecificValuesToUpdate: {
+          quantity: (currentQuantity: number) =>
+            currentQuantity > 0 ? currentQuantity + 1 : 1,
+        },
+      }),
+    );
+  }, []);
+
+  //   const onSwipeLeft = useCallback(
+  //     (key: Key) => {
+  //       dispatch(
+  //         updateStoreSpecificValues({
+  //           key,
+  //           storeSpecificValuesToUpdate: {
+  //             isInCart: (current) => false,
+  //             quantity: (current) => 0,
+  //           },
+  //         }),
+  //       );
+  //       LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+  //     },
+  //     [listRef],
+  //   );
+
+  function renderItem({ item, index }: ListRow<ItemWithStoreSpecificValues>) {
+    // const key = {
+    //   name: item.name,
+    //   upc: item.upc,
+    // } as Key;
+    const keyToUse = getKeyToUse(item);
+    const now = Date.now();
+    const lastPurchaseDate = lastPurchasedMap[keyToUse]?.[currentStore.name];
+    const isRecommended =
+      item.frequency &&
+      lastPurchaseDate &&
+      lastPurchaseDate + item.frequency <= now;
+
+    return (
+      <SwipeableRow
+        key={index}
+        rightSwipe={{
+          backgroundColor: theme.colors.primary[900],
+          onPress: onSwipeRight.bind(null, item),
+          title: currentStore.name ? (
+            <Stack
+              paddingLeft={theme.space[FORM_INTER_ITEM_SPACING]}
+              alignItems="center"
+            >
+              <FontAwesome
+                name="plus"
+                color={theme.colors.white}
+                size={theme.sizes[6]}
+              />
+              <Text color={theme.colors.white}>To Cart</Text>
+            </Stack>
+          ) : (
+            <Text
+              width={150}
+              numberOfLines={2}
+              paddingLeft={theme.space[FORM_INTER_ITEM_SPACING]}
+              color={theme.colors.white}
+            >
+              Select a Store to Add to Shopping List
+            </Text>
+          ),
+        }}
+      >
+        <ItemTileForRecommendedItems
+          isRecommended={!!isRecommended}
+          isMultiSelectMode={isMultiSelectMode}
+          listName={listName}
+          item={item}
+          buttonProps={{
+            onLongPress: () => {
+              dispatch(
+                updateSelectedItemsFromPreviouslyPurchased({
+                  operation: 'set',
+                  item: isMultiSelectMode ? undefined : item,
+                }),
+              );
+              dispatch(
+                setIsMultiSelectModeForPreviouslyPurchased(!isMultiSelectMode),
+              );
+            },
+          }}
+          onSelect={(item) => {
+            const isSelected = !!selectedItems.find(
+              (itemLocal) => getKeyToUse(item) === getKeyToUse(itemLocal),
+            );
+            if (isSelected) {
+              dispatch(
+                updateSelectedItemsFromPreviouslyPurchased({
+                  operation: 'remove',
+                  item,
+                }),
+              );
+            } else {
+              dispatch(
+                updateSelectedItemsFromPreviouslyPurchased({
+                  operation: 'add',
+                  item,
+                }),
+              );
+            }
+          }}
+          isSelected={
+            !!selectedItems.find(
+              (itemLocal) => getKeyToUse(item) === getKeyToUse(itemLocal),
+            )
+          }
+        />
+      </SwipeableRow>
+    );
+  }
+
+  return (
+    <FlashList
+      ref={listRef}
+      refreshing={refreshing}
+      onRefresh={() => {
+        setRefreshing(true);
+        setTimeout(() => {
+          setRefreshing(false);
+        }, 2000);
+      }}
+      data={itemsPurchasedAtStore}
+      renderItem={renderItem}
+      keyExtractor={(item: ItemWithStoreSpecificValues, index: number) =>
+        getKeyToUse(item)
+      }
+      estimatedItemSize={150}
+      ItemSeparatorComponent={() => <ListItemSeparator />}
+    />
+  );
+}

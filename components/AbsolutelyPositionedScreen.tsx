8f2910fd1d4bee647e115d6c862adfe4f9513a6c
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-04-19 15:13:37 -0500

    Add useFlatlist in AbsolutelytPositionedScreen

--- components/AbsolutelyPositionedScreen.tsx
+++ components/AbsolutelyPositionedScreen.tsx
@@ -1,4 +1,4 @@
-import { Column, ScrollView, View, useTheme } from 'native-base';
+import { Column, FlatList, ScrollView, View, useTheme } from 'native-base';
 import { ReactNode, useState } from 'react';
 
 import { FORM_INTER_ITEM_SPACING } from '@/constants/general';
@@ -7,21 +7,41 @@ import { absolutePositioning } from '@/constants/styles';
 type AbsolutePositionedScreenProps = {
   absolutelyPositionedJsx: ReactNode | ReactNode[];
   children: ReactNode | ReactNode[];
+  useFlatList?: boolean;
 };
 
 const CONTAINER_HEIGHT_DEFAULT = 0;
 export function AbsolutePositionedScreen(props: AbsolutePositionedScreenProps) {
-  const { absolutelyPositionedJsx, children } = props;
+  const { absolutelyPositionedJsx, children, useFlatList } = props;
   const [containerHeight, setContainerHeight] = useState(
     CONTAINER_HEIGHT_DEFAULT,
   );
   const theme = useTheme();
 
+  function renderList() {
+    const contentJSX = <View pb={theme.sizes[1]}>{children}</View>;
+    const commonStyles = {
+      m: theme.space[1],
+      mt: 0,
+      mb: containerHeight,
+    };
+
+    if (useFlatList) {
+      return (
+        <FlatList
+          {...commonStyles}
+          data={[{ jsx: contentJSX, index: 1 }]}
+          keyExtractor={(item) => item.index.toString()}
+          renderItem={(item) => item.item.jsx}
+        />
+      );
+    }
+    return <ScrollView {...commonStyles}>{contentJSX}</ScrollView>;
+  }
+
   return (
     <Column {...absolutePositioning}>
-      <ScrollView m={theme.space[1]} mt={0} mb={containerHeight}>
-        <View pb={theme.sizes[1]}>{children}</View>
-      </ScrollView>
+      {renderList()}
       <Column
         {...absolutePositioning}
         top="auto"

8649bff26919847ae112aefa8612672fcab34a95
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-05-01 15:55:18 -0500

    Add a better way to specify how nameOrderTemplate is created (use drop down with a list of currently used fields PR)

--- TODOS.md
+++ TODOS.md
@@ -1,5 +1,4 @@
 ## MVP
--Add a better way to specify how nameOrderTemplate is created (use drop down with a list of currently used fields PR)
 -focus on name field when adding a new store (but not when viewing existing store)
 
 -add a button/menu option in shopping tab to for "shopping mode", which opens a drawer that scans barcodes and then moves that barcode (if found in shopping list to in cart list).
--- components/options/NameOrderSpecifier.tsx
+++ components/options/NameOrderSpecifier.tsx
@@ -1,8 +1,13 @@
-import { Column, Input, Row, Text, useTheme } from 'native-base';
-import React, { useEffect, useRef, useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
+import { Column, Heading, Input, Row, useTheme } from 'native-base';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { FORM_INTER_ITEM_SPACING } from '@/constants/general';
+import {
+  FORM_INTER_ITEM_SPACING,
+  NAME_ORDER_BRANDS_STRING,
+  NAME_ORDER_PRODUCT_NAME_STRING,
+} from '@/constants/general';
 import {
   nameOrderTemplateSelector,
   setNameOrderTemplate,
@@ -11,13 +16,27 @@ import {
 type NameOrderSpecifierProps = object;
 
 const DEBOUNCE_TIMEOUT = 366;
+const ALLOWED_FIELDS = [
+  'Add a field',
+  NAME_ORDER_BRANDS_STRING,
+  NAME_ORDER_PRODUCT_NAME_STRING,
+];
+
 export function NameOrderSpecifier(props: NameOrderSpecifierProps) {
   const theme = useTheme();
   const dispatch = useDispatch();
   const nameOrderTemplate = useSelector(nameOrderTemplateSelector);
   const [nameOrderTemplateValue, setNameOrderTemplateValue] =
     useState(nameOrderTemplate);
+  const [selectedPickerValue, setSelectedPickerValue] = useState<string>(
+    ALLOWED_FIELDS[0],
+  );
   const debounceTimeoutRef = useRef<any>();
+
+  const onPickerValueChange = useCallback((newValue: string) => {
+    setSelectedPickerValue(ALLOWED_FIELDS[0]);
+    setNameOrderTemplateValue((current) => current + newValue);
+  }, []);
 
   useEffect(() => {
     clearTimeout(debounceTimeoutRef.current);
@@ -27,14 +46,32 @@ export function NameOrderSpecifier(props: NameOrderSpecifierProps) {
   }, [nameOrderTemplateValue]);
 
   return (
-    <Column>
+    <Column my={theme.space[FORM_INTER_ITEM_SPACING]}>
+      <Heading size="sm" textAlign="center">
+        Name Order Template:
+      </Heading>
       <Row alignItems="center" space={theme.space[FORM_INTER_ITEM_SPACING]}>
-        <Text>Name Order Template:</Text>
-        <Input
-          value={nameOrderTemplateValue}
-          onChangeText={(newValue) => setNameOrderTemplateValue(newValue)}
-          flex={1}
-        />
+        <Column flex={1}>
+          <Picker
+            style={{ flex: 1 }}
+            selectedValue={selectedPickerValue}
+            onValueChange={onPickerValueChange}
+          >
+            {Object.values(ALLOWED_FIELDS).map((allowedField) => (
+              <Picker.Item
+                key={allowedField}
+                label={allowedField}
+                value={allowedField}
+              />
+            ))}
+          </Picker>
+        </Column>
+        <Column flex={1}>
+          <Input
+            value={nameOrderTemplateValue}
+            onChangeText={(newValue) => setNameOrderTemplateValue(newValue)}
+          />
+        </Column>
       </Row>
     </Column>
   );

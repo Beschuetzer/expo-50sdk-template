c6f832a131e20f98a21b0f1170bf5fbfb70b94b7
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-05-01 13:31:34 -0500

    setup debouncing

--- components/options/NameOrderSpecifier.tsx
+++ components/options/NameOrderSpecifier.tsx
@@ -1,22 +1,34 @@
 import { Column, Input, Row, Text, useTheme } from 'native-base';
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { FORM_INTER_ITEM_SPACING } from '@/constants/general';
-import { nameOrderTemplateSelector } from '@/state/slices/optionsSlice';
+import {
+  nameOrderTemplateSelector,
+  setNameOrderTemplate,
+} from '@/state/slices/optionsSlice';
 
 type NameOrderSpecifierProps = object;
 
+const DEBOUNCE_TIMEOUT = 366;
 export function NameOrderSpecifier(props: NameOrderSpecifierProps) {
-  const {} = props;
   const theme = useTheme();
+  const dispatch = useDispatch();
   const nameOrderTemplate = useSelector(nameOrderTemplateSelector);
   const [nameOrderTemplateValue, setNameOrderTemplateValue] =
     useState(nameOrderTemplate);
+  const debounceTimeoutRef = useRef<any>();
+
+  useEffect(() => {
+    clearTimeout(debounceTimeoutRef.current);
+    debounceTimeoutRef.current = setTimeout(() => {
+      dispatch(setNameOrderTemplate(nameOrderTemplateValue));
+    }, DEBOUNCE_TIMEOUT);
+  }, [nameOrderTemplateValue]);
 
   return (
     <Column>
-      <Row alignItems={"center"} space={theme.space[FORM_INTER_ITEM_SPACING]}>
+      <Row alignItems="center" space={theme.space[FORM_INTER_ITEM_SPACING]}>
         <Text>Name Order Template:</Text>
         <Input
           value={nameOrderTemplateValue}

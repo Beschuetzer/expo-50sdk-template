6c9446ab3348a27339249ad470b798d4f4e874c9
Adam <62818816+Beschuetzer@users.noreply.github.com>
2026-09-10 15:16:48 -0500

    chore: working

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -3,15 +3,17 @@ import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import { Platform } from 'react-native';
 
+const ExternalLinkComponent = Link as React.ComponentType<any>;
+
 export function ExternalLink(
   props: Omit<React.ComponentProps<typeof Link>, 'href'> & { href: string },
 ) {
   return (
-    <Link
+    <ExternalLinkComponent
       target="_blank"
       {...props}
       href={props.href as React.ComponentProps<typeof Link>['href']}
-      onPress={(e) => {
+      onPress={(e: { preventDefault: () => void }) => {
         if (Platform.OS !== 'web') {
           // Prevent the default behavior of linking to the default browser on native.
           e.preventDefault();
